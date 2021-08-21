5a07b81fcc7ca790831159cd5be12a2df39ae628
Brian Payne <brianpayne8699@gmail.com>
2021-08-21 16:50:44 -0500

    Errors and cleanup

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -1,32 +1,34 @@
-import React from 'react'
-import Square from './Square'
-import { Container, Row, Col } from 'reactstrap';
-
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Square from './Square';
 
 class Board extends Component {
-constructor(props) {
-    super(props)
-
-    this.state = {
-        board: ["", "", "", "", "", "", "", "", "",]
+    renderSquare(i){
+        return <Square value={this.props.squares[i]}
+        onClick={()=>this.props.onClick(i)}
+        />
+    }
+    render() {
+        return (
+            <div>
+                <div className="border-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="border-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="border-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+                
+            </div>
+        )
     }
 }
 
-
-    render() {
-        return (
-          <div className="board">
-              {this.state.board.map({Square}) => {
-                return (<div onClick={() => this.state.Player(index)} className="square">{Square}</div>)
-              })}
-            </div>        
-          
-        );
-      }
-    
-
-}
-
-export default Board
-                    
+export default Board;
--- src/Components/Square.js
+++ src/Components/Square.js
@@ -1,37 +1,9 @@
-import React, { Component } from 'react'
-import { Card, CardText, CardBody } from 'reactstrap'
-import Player from './Player'
-class Square extends Component {
-    constructor(props) {
-        super(props)
-    
-        this.state = {
-            Player
-             
-        }
-        this.squareClicked = this.squareClicked.bind(this)
-  }
+import React from 'react'
 
-
-squareClicked(index) {
-  let player_turn = this.state.player_turn
-  let board = this.state.board
-    }
-    
-    render() {
-        return (
-            <div>
-                <Card size='sm' text='white' color='#36739B'>
-                    <CardBody class="card-body" />
-                </Card>
-            </div>
-        )
-    }
+export default function Square(props) {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    )
 }
-
-export default Square;
-            
-                    
-                    
-                    
-            
